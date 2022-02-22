d91ec53075d323d3191f837b35608a5496ec2da5
Hamshita Kancharlapalli <hamshita11@gmail.com>
2022-02-22 10:39:03 +0530

    update Readme.md

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -6,20 +6,23 @@ import warnings
 import sys
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+#from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import StandardScaler 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import precision_recall_curve, auc
+from sklearn.metrics import confusion_matrix
 from get_data import read_params
 import argparse
 import joblib
 import json
 
 
-def eval_metrics(actual, pred):
+"""def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+    return rmse, mae, r2"""
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
@@ -28,8 +31,8 @@ def train_and_evaluate(config_path):
     random_state = config["base"]["random_state"]
     model_dir = config["model_dir"]
 
-    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
-    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+    alpha = config["train_and_evaluate"]["alpha"]
+    #l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
 
     target = [config["base"]["target_col"]]
 
@@ -42,47 +45,36 @@ def train_and_evaluate(config_path):
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
 
-    lr = ElasticNet(
-        alpha=alpha, 
-        l1_ratio=l1_ratio, 
-        random_state=random_state)
-    lr.fit(train_x, train_y)
+    sc = StandardScaler()
+    train_x = sc.fit_transform(train_x)
+    test_x = sc.fit_transform(test_x)
 
-    predicted_qualities = lr.predict(test_x)
-    
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    classifier = GaussianNB()
+    classifier.fit(train_x , train_y)
 
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+    predicted_qualities = classifier.predict(test_x)
+    predictions = predicted_qualities[:,-1]
+    cm = confusion_matrix(test_y , predicted_qualities)
+
+    # generate scores
+    precision, recall, thresholds = precision_recall_curve(test_y, predictions)
+    auc = auc(recall, precision)
 
 #####################################################
     scores_file = config["reports"]["scores"]
     params_file = config["reports"]["params"]
 
     with open(scores_file, "w") as f:
-        scores = {
-            "rmse": rmse,
-            "mae": mae,
-            "r2": r2
-        }
-        json.dump(scores, f, indent=4)
+        json.dump({'auc': auc}, f)
 
     with open(params_file, "w") as f:
-        params = {
-            "alpha": alpha,
-            "l1_ratio": l1_ratio,
-        }
-        json.dump(params, f, indent=4)
-#####################################################
-
-
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
-
-    joblib.dump(lr, model_path)
-
+        proc_dict = {'proc': [{
+        'precision': p,
+        'recall': r,
+        'threshold': t
+        } for p, r, t in zip(precision, recall, thresholds)
+    ]}
+    json.dump(proc_dict, f)
 
 
 if __name__=="__main__":
